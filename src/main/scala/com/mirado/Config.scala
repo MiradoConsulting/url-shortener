fc0f636096400c7e8f7cf24ff688b741719e62a0
John Haynes <john.andrew.haynes@gmail.com>
2018-09-02 14:37:58 +0200

    remove inputfile conf param

--- src/main/scala/com/mirado/Config.scala
+++ src/main/scala/com/mirado/Config.scala
@@ -1,17 +1,15 @@
 package com.mirado
 
-case class Config(inputFile: String,
-                  cassandraHost: String,
+case class Config(cassandraHost: String,
                   urlPrefix: String)
 
 object Configuration {
 
     def readConfig =
         for {
-            inputFile <- readConfigVal("INPUT_FILE").right
             cassandraHost <- readConfigVal("CASSANDRA_HOST").right
             urlPrefix <- readConfigVal("URL_PREFIX").right
-        } yield Config(inputFile, cassandraHost, urlPrefix)
+        } yield Config(cassandraHost, urlPrefix)
 
     private def readConfigVal(key: String) =
         sys.env.get(key) match {
